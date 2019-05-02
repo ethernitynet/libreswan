5a44ef8096f96ca6547a045fffc93b562b0c1f8d
root <erezb@ethernitynet.com>
2019-05-02 07:52:03 -0400

    add ip-neigh

--- programs/pluto/enet_tunnel.c
+++ programs/pluto/enet_tunnel.c
@@ -13,10 +13,13 @@ const char outbound_tunnel_add_format[] =
 	},\n\
 	\"ipsec_cfg\": {\n\
 		\"spi\": %u,\n\
-		\"auth_algo\": %s,\n\
+		\"auth_algo\": \"%s\",\n\
 		\"cipher_algo\": \"%s\",\n\
 		\"auth_key\": \"%s\",\n\
 		\"cipher_key\": \"%s\"\n\
+	},\n\
+	\"meta\": {\n\
+		\"ip_neigh\": %s\n\
 	}\n\
 }\n";
 
@@ -30,10 +33,13 @@ const char inbound_tunnel_add_format[] =
 	},\n\
 	\"ipsec_cfg\": {\n\
 		\"spi\": %u,\n\
-		\"auth_algo\": %s,\n\
+		\"auth_algo\": \"%s\",\n\
 		\"cipher_algo\": \"%s\",\n\
 		\"auth_key\": \"%s\",\n\
 		\"cipher_key\": \"%s\"\n\
+	},\n\
+	\"meta\": {\n\
+		\"ip_neigh\": %s\n\
 	}\n\
 }\n";
 
@@ -66,11 +72,38 @@ static const char *key_to_str(char *key_str, const unsigned char *key, const uns
 	return key_str_head;
 };
 
+static const char *ip_neigh_to_str(char *ip_neigh_str) {
+	
+	const char *ip_neigh_str_head = ip_neigh_str;
+	char pline[1024];
+	FILE *fpipe = (FILE *)popen("ip neigh", "r");
+	
+	ip_neigh_str += sprintf(ip_neigh_str, "[\n");
+	fflush(fpipe);
+	if(fgets(pline, sizeof(pline), fpipe)) {
+		if(pline[strlen(pline) - 1] == '\n') {
+			pline[strlen(pline) - 1] = '\0';
+		};
+		ip_neigh_str += sprintf(ip_neigh_str, "\"%s\"\n", pline);
+	};
+	while(fgets(pline, sizeof(pline), fpipe)) {
+		if(pline[strlen(pline) - 1] == '\n') {
+			pline[strlen(pline) - 1] = '\0';
+		};
+		ip_neigh_str += sprintf(ip_neigh_str, ",\"%s\"\n", pline);
+	};
+	fflush(fpipe);
+	pclose(fpipe);
+	ip_neigh_str += sprintf(ip_neigh_str, "]");
+	return ip_neigh_str_head;
+};
+
 void enet_tunnel_config_apply(const char *uri, const enet_tunnel_config *config) {
 	
 	char cmd_str[2048];
 	char auth_key_str[1024];
 	char cipher_key_str[1024];
+	char ip_neigh_str[4096];
 		
 	const char *tunnel_config_format = NULL;
 	switch(config->config_type) {
@@ -89,7 +122,8 @@ void enet_tunnel_config_apply(const char *uri, const enet_tunnel_config *config)
 		config->auth_algo,
 		config->cipher_algo,
 		key_to_str(auth_key_str, config->auth_key, config->auth_keylen),
-		key_to_str(cipher_key_str, config->cipher_key, config->cipher_keylen)
+		key_to_str(cipher_key_str, config->cipher_key, config->cipher_keylen),
+		ip_neigh_to_str(ip_neigh_str)
 		);
 	enet_tunnel_cmd(uri, cmd_str);
 };
